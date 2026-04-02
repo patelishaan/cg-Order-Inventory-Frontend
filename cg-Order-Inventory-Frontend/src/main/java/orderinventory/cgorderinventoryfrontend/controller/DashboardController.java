06b2d5074ca5dec7565e7e08952b4760b321455b
g1tmaster <adityaraj4160@gmail.com>
2026-04-02 14:53:11 +0530

    added order page

--- cg-Order-Inventory-Frontend/src/main/java/orderinventory/cgorderinventoryfrontend/controller/DashboardController.java
+++ cg-Order-Inventory-Frontend/src/main/java/orderinventory/cgorderinventoryfrontend/controller/DashboardController.java
@@ -38,10 +38,27 @@ public class DashboardController {
         model.addAttribute("backendUrl", backendUrl);
         return "store";
     }
+    
+    
 
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
         return "redirect:/auth/login";
     }
+    
+    @GetMapping("/order")
+    public String order(HttpSession session, Model model) {
+        String token = (String) session.getAttribute("jwt");
+
+        if (token == null) return "redirect:/auth/login";
+
+        model.addAttribute("email", session.getAttribute("email"));
+        model.addAttribute("role", session.getAttribute("role"));
+        model.addAttribute("token", token);
+        model.addAttribute("backendUrl", backendUrl);
+
+        return "order";
+    }
+    
 }
